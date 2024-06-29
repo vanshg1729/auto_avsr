4c158d20a37670f8d2d3c6b2985b0ed1fc063bb5
fine-man <spacetime299@gmail.com>
2024-06-29 18:13:08 +0530

    made changes to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 python demo.py  data.modality=video \
                 pretrained_model_path=./checkpoints/vsr_trlrwlrs2lrs3vox2avsp_base.pth \
-                file_path=./data/leah_crop.mp4
+                file_path=./data/vansh/1718881781149_0_66740df5bcb54392537d19a8.mp4
