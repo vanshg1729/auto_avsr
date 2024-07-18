525305c738801fcafde5785f0aef588593c2e969
fine-man <spacetime299@gmail.com>
2024-07-19 01:01:58 +0530

    added batch face detection in process_lip2wav.py

---
+++ sbatch_files/process_lip2wav.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -A research
+#SBATCH --nodelist=gnode071
+#SBATCH --gres=gpu:4
+#SBATCH --mem-per-cpu=2G
+#SBATCH -c 36
+#SBATCH --time=96:00:00
+#SBATCH --mail-user=vansh.garg@research.iiit.ac.in
+#SBATCH --mail-type=ALL
+
+cd ~/auto_avsr
+git checkout personalization
+
+python3 data_processing/process_lip2wav.py --ngpu 4 --speaker eh
