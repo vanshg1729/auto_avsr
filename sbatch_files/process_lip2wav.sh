50aa3cbbfd3f0c7ba9267e91f5d515c6f3f233b1
vanshg <spacetime299@gmail.com>
2024-07-19 01:11:09 +0530

    made some random filename changes

---
+++ sbatch_files/process_lip2wav.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -A vanshg
+#SBATCH --nodelist=gnode079
+#SBATCH --gres=gpu:4
+#SBATCH --mem-per-cpu=2G
+#SBATCH -c 36
+#SBATCH --time=96:00:00
+#SBATCH --mail-user=vansh.garg@research.iiit.ac.in
+#SBATCH --mail-type=ALL
+
+cd ~/auto_avsr
+git checkout personalization
+
+python3 data_processing/process_lip2wav.py --ngpu 4 --speaker dl
